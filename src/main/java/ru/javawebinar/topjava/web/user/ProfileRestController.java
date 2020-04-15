63320f482e58a4d0e81f6dab34a2c404ef15047e
admin@javaops.ru <gitlaB13>
2020-04-16 00:15:40 +0300

    10_09_registration

--- src/main/java/ru/javawebinar/topjava/web/user/ProfileRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/ProfileRestController.java
@@ -2,9 +2,13 @@ package ru.javawebinar.topjava.web.user;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.to.UserTo;
+
+import java.net.URI;
 
 import static ru.javawebinar.topjava.web.SecurityUtil.authUserId;
 
@@ -24,7 +28,19 @@ public class ProfileRestController extends AbstractUserController {
         super.delete(authUserId());
     }
 
-    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public ResponseEntity<User> register(@RequestBody UserTo userTo) {
+        User created = super.create(userTo);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL).build().toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @PutMapping(consumes =
+
+
+            MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(@RequestBody UserTo userTo) {
         super.update(userTo, authUserId());
